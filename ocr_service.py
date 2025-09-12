6ca8d5ee7cd366f6db204ad27864608b331730cb
kcm <>
2025-09-12 21:53:48 +0900

    server js

--- ocr_service.py
+++ ocr_service.py
@@ -27,4 +27,5 @@ def run_ocr():
     return jsonify({"ocr_text": " ".join(texts)})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port)
