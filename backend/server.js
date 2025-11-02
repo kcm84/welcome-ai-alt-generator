712d4a14c142c8a238fb2f7d0a4e43da889c8856
kcm <>
2025-11-02 17:57:41 +0900

    server js

--- backend/server.js
+++ backend/server.js
@@ -92,7 +92,7 @@ async function generateAltTag(imagePath) {
 
     // Hugging Face Router 호출
     const chatCompletion = await client.chat.completions.create({
-      model: "Qwen/Qwen3-VL-8B-Instruct", //"Qwen/Qwen2.5-VL-7B-Instruct:hyperbolic",
+      model: "Qwen/Qwen2.5-VL-7B-Instruct:hyperbolic",
       messages: [
         {
           role: "system",
